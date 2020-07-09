cdc6401eca93801906d74a4c4746ae860871a4a9
David Munoz Constantine <dmunoz.dmc@gmail.com>
2020-07-09 09:04:33 -0700

    reformatted code to pep8 standard

--- nbg_lr_classifier.py
+++ nbg_lr_classifier.py
@@ -1,102 +1,145 @@
+"""Demonstration of a logistic regression and naive gaussian classifier.
+
+It uses the spambase dataset.
+"""
+
+
 import numpy as np
 import csv
 import math
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 from sklearn.linear_model import LogisticRegression
-import sys
 
-EPSILON  = 0.0001
-
-
-def logisticRegression(trainingFeatureData, testFeatureData, trainingClassType):
-  model = LogisticRegression(solver='liblinear', multi_class='ovr')
-  model.fit(trainingFeatureData, trainingClassType)
-  
-  return model.predict(testFeatureData)
+# Avoids division by zero.
+EPSILON = 0.001
 
 
-#returns stdev and mean of the features based on the class given 
-#and the features list. 
-def gaussianStatsByClass(featuresList, classList, classType):
-  #get locations of features based on the classType
-  indicesOfClass = np.nonzero(classList != classType)[0]
-  #remove the non desired classes from features
-  result = np.delete(featuresList, indicesOfClass, axis=0)
-  
-  return result.std(axis=0) + EPSILON, result.mean(axis=0)
+def logistic_regresssion(training_features, test_features, training_classes):
+    """Wrapper function for Scikit's Logistic Regression"""
+    model = LogisticRegression(solver="liblinear", multi_class="ovr")
+    model.fit(training_features, training_classes)
+    return model.predict(test_features)
 
-def naiveGaussianClassifier(trainingFeatureData, testFeatureData, trainingClassType, 
-                            probabilityClassType):
-  predictedClass= []
-  #get gaussian probability statistics for each class
-  stdSpam, meanSpam = gaussianStatsByClass(trainingFeatureData, trainingClassType, 1)
-  stdNotSpam, meanNotSpam = gaussianStatsByClass(trainingFeatureData, trainingClassType, 0)
-  #for each feature calculate its probability based on the gaussian formulas
-  for i in range(len(testFeatureData)):
-    probabilitySpam =math.log10(probabilityClassType[0]) + \
-        np.sum(np.log10((1/(math.sqrt(2*math.pi)*stdSpam)) * \
-                        np.exp(-(np.power(testFeatureData[i]-meanSpam,2)/(2*np.power(stdSpam,2))))))
-    probabilityNotSpam = math.log10(probabilityClassType[0]) + \
-        np.sum(np.log10((1/(math.sqrt(2*math.pi)*stdNotSpam)) * \
-                        np.exp(-(np.power(testFeatureData[i]-meanNotSpam,2)/(2*np.power(stdNotSpam,2))))))
-    #choose the result based on which probability is greater
-    predClassType = 1 if probabilitySpam > probabilityNotSpam else 0
-    predictedClass.append(predClassType)
-    
-  return np.asarray(predictedClass)
-  
-def computePriorProbability(classList, classType):
-  #sets all elements in class list that are equal to classType to True, else is False
-  classList = classList == classType
-  #return instances of classType by class list length, which is the probability 
-  return np.count_nonzero(classList)/len(classList)
-    
+
+def gaussian_stats_by_class(featuresList, classList, class_types):
+    """Returns stdev and mean of a given class only."""
+    # Get locations of features based on the class_types.
+    indices_class = np.nonzero(classList != class_types)[0]
+    # Remove the non desired classes from features.
+    result = np.delete(featuresList, indices_class, axis=0)
+    return result.std(axis=0) + EPSILON, result.mean(axis=0)
+
+
+def naive_gaussian_classifier(
+    training_features, test_features, training_classes, probability_class,
+):
+    """Based on naive gaussian probability, return predictions."""
+    predicted_class = []
+    # Get gaussian probability statistics for each class.
+    std_spam, mean_spam = gaussian_stats_by_class(
+        training_features, training_classes, 1
+    )
+    std_not_spam, mean_not_spam = gaussian_stats_by_class(
+        training_features, training_classes, 0
+    )
+    # For each feature calculate its probability based on the gaussian
+    # formulas.
+    for i in range(len(test_features)):
+        probability_spam = math.log10(probability_class[0]) + np.sum(
+            np.log10(
+                (1 / (math.sqrt(2 * math.pi) * std_spam))
+                * np.exp(
+                    -(
+                        np.power(test_features[i] - mean_spam, 2)
+                        / (2 * np.power(std_spam, 2))
+                    )
+                )
+            )
+        )
+        probability_not_spam = math.log10(probability_class[0]) + np.sum(
+            np.log10(
+                (1 / (math.sqrt(2 * math.pi) * std_not_spam))
+                * np.exp(
+                    -(
+                        np.power(test_features[i] - mean_not_spam, 2)
+                        / (2 * np.power(std_not_spam, 2))
+                    )
+                )
+            )
+        )
+        # Choose the result based on which probability is greater.
+        pred = 1 if probability_spam > probability_not_spam else 0
+        predicted_class.append(pred)
+
+    return np.array(predicted_class)
+
+
+def prior_probability(classes, class_types):
+    """Computes prior probabilities."""
+    class_list = classes == class_types
+    return np.count_nonzero(class_list) / len(classes)
+
 
 def main():
-  
-  features = []
-  classType = []
-  dataFile = open(sys.argv[1])
-  dataReader = csv.reader(dataFile, delimiter=',')
-  #append all features and classes from csv file
-  for row in dataReader:
-    features.append([float(i) for i in row[0:57]])
-    classType.append(int(row[-1]))
-  
-  features = np.asarray(features)
-  classType = np.asarray(classType)
-  #use Scikit's built in data splitter 
-  trainingFeatureData, testFeatureData,trainingClassType, testClassType = \
-          train_test_split(features,classType, test_size=.5, random_state=None) 
-  #compute prior probablities of the data
-  trainingProbabilitySpam = computePriorProbability(trainingClassType, 1)
-  trainingProbabilityNotSpam = computePriorProbability(trainingClassType, 0)
-  
-  print("Naive Gaussian Classifier Result")
-  #get the predicted classes from the gaussian classifier
-  predictedClass = naiveGaussianClassifier(trainingFeatureData, testFeatureData,
-                                           trainingClassType, 
-                                           [trainingProbabilityNotSpam,trainingProbabilitySpam])
-  #create the confusion matrix based on the predicted classes and the actual data
-  #and print metrics
-  cm = confusion_matrix(testClassType, predictedClass)
-  print(cm)
-  tn, fp, fn, tp = cm.ravel()
-  print("Accuracy", (tp+tn)/(tn+tp+fp+fn), "precision", tp/(tp+fp), "recall", tp/(tp+fn))
-  
-  print("Logistic Regression Classifier Result")
-  #get the predicted classes from Scikit's logistic regression classifier
-  predictedClass = logisticRegression(trainingFeatureData, testFeatureData, trainingClassType)
-  #create the confusion matrix based on the predicted classes and the actual data
-  #and print metrics
-  cm = confusion_matrix(testClassType, predictedClass)
-  print(cm)
-  tn, fp, fn, tp = cm.ravel()
-  print("Accuracy", (tp+tn)/(tn+tp+fp+fn), "precision", tp/(tp+fp), "recall", tp/(tp+fn))
-  
-if __name__ == '__main__':
-  if len(sys.argv) > 1:
+    features = []
+    class_types = []
+    data_reader = csv.reader(open("spambase.csv"), delimiter=",")
+    # Append all features and classes from csv file.
+    for row in data_reader:
+        features.append([float(i) for i in row[0:57]])
+        class_types.append(int(row[-1]))
+
+    features = np.array(features)
+    class_types = np.array(class_types)
+    # use Scikit's built in data splitter.
+    (
+        training_features,
+        test_features,
+        training_classes,
+        test_classes,
+    ) = train_test_split(
+        features, class_types, test_size=0.5, random_state=None
+    )
+    training_probability_spam = prior_probability(training_classes, 1)
+    training_probability_not_spam = prior_probability(training_classes, 0)
+    print("Naive Gaussian Classifier Result")
+    predicted_class = naive_gaussian_classifier(
+        training_features,
+        test_features,
+        training_classes,
+        [training_probability_not_spam, training_probability_spam],
+    )
+    print("Confusion Matrix")
+    cm = confusion_matrix(test_classes, predicted_class)
+    print(cm)
+    tn, fp, fn, tp = cm.ravel()
+    print(
+        "Accuracy",
+        (tp + tn) / (tn + tp + fp + fn),
+        "precision",
+        tp / (tp + fp),
+        "recall",
+        tp / (tp + fn),
+    )
+
+    print("Logistic Regression Classifier Result")
+    predicted_class = logistic_regresssion(
+        training_features, test_features, training_classes
+    )
+    print("Confusion Matrix")
+    cm = confusion_matrix(test_classes, predicted_class)
+    print(cm)
+    tn, fp, fn, tp = cm.ravel()
+    print(
+        "Accuracy",
+        (tp + tn) / (tn + tp + fp + fn),
+        "precision",
+        tp / (tp + fp),
+        "recall",
+        tp / (tp + fn),
+    )
+
+
+if __name__ == "__main__":
     main()
-  else:
-    print("Needs to provide spambase.csv as argument")
--- svm_classifier.py
+++ svm_classifier.py
@@ -1,3 +1,9 @@
+"""Demonstration of a support vector machine classifier.
+
+It uses the spambase dataset. Using the top 2 features it achieves an
+accuracy of ~75%. Using all features it reaches ~92%.
+"""
+
 from sklearn import svm
 from sklearn import preprocessing
 from sklearn.metrics import accuracy_score, precision_score, recall_score
@@ -5,145 +11,210 @@ from sklearn.metrics import roc_curve, auc
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
-import random
-import sys
 from sklearn.model_selection import train_test_split
 
 
-def featuresTraining(trainingFeatureData, testFeatureData, trainingClassType, 
-                     testClassType, selectionIndices):
-  #get the first training and test feature data based on the first index
-  trainingData = np.array([trainingFeatureData[:,selectionIndices[0]]])
-  testData = np.array([testFeatureData[:,selectionIndices[0]]])
-  
-  #iterate over all features
-  for m in range(1, 57, 1):
+def features_training(
+    training_features,
+    test_features,
+    training_classes,
+    test_classes,
+    feature_selection,
+):
+    """Perform SVM linear classification. Return accuracies.
 
-    #append the next feature
-    trainingData = np.append(trainingData, 
-                             np.array([trainingFeatureData[:,selectionIndices[m]]]), axis=0)
-    testData = np.append(testData, 
-                         np.array([testFeatureData[:,selectionIndices[m]]]), axis=0)
+    Given the features in order of preference, perform SVM training and
+    get accuracy.
+    It starts with the top 2 features, then top 3, etc, until using all
+    features.
+    """
+    accuracies = []
+    # Get the first training and test feature data based on the first index.
+    training_data = np.array([training_features[:, feature_selection[0]]])
+    test_data = np.array([test_features[:, feature_selection[0]]])
+    # For all features, perform classification.
+    for i in range(1, len(training_features[0])):
+        # Append next features.
+        training_data = np.append(
+            training_data,
+            np.array([training_features[:, feature_selection[i]]]),
+            axis=0,
+        )
+        test_data = np.append(
+            test_data,
+            np.array([test_features[:, feature_selection[i]]]),
+            axis=0,
+        )
 
-    #transpose these data so features are in columns instead of rows
-    trainingDataT = trainingData.T
-    testDataT = testData.T
-    #get stdev and mean for scaling test data
-    stdDevTraining = trainingDataT.std(axis=0)
-    meanTraining = trainingDataT.mean(axis=0)
-    
-    #Use scikit's preprocessing module to scale training data
-    trainingDataS = preprocessing.scale(trainingDataT)
-    #Scale test data based on mean and stdev of training data
-    testDataS = np.array(testDataT, copy=True)
-    for i in range(len(testDataS)):
-      testDataS[i] = (testDataS[i] - meanTraining)/stdDevTraining
-      
-    #use a linear kernel
-    clf = svm.SVC(kernel='linear')
-    clf.fit(trainingDataS, trainingClassType) 
-    prediction = clf.predict(testDataS)
-    accuracy = accuracy_score(testClassType, prediction)
-    #print accuracy of the training of the selected features
-    print(accuracy)
-
-def randomFeatureSelection(trainingFeatureData, testFeatureData, trainingClassType, testClassType):
-  randomIndices = [x for x in range(57)]
-  #randomize indices to access when training
-  for _ in range(7):
-    random.shuffle(randomIndices)
-  #perform features training based on these random indices
-  featuresTraining(trainingFeatureData, testFeatureData, trainingClassType, testClassType, np.array(randomIndices))  
- 
- 
- 
-def weightFeatureSelection(trainingFeatureData, testFeatureData, trainingClassType, testClassType, weightVector):
-  #get the sorted indices of the weights vector
-  sortedWeightsIndices = np.argsort(weightVector)[::-1]
-  #perform features training based on these sorted indices
-  featuresTraining(trainingFeatureData, testFeatureData, trainingClassType, testClassType, sortedWeightsIndices)
-
+        # Transpose these data so features are in columns instead of rows.
+        training_data_t = training_data.T
+        test_data_t = test_data.T
+        # Get stdev and mean for test data. Used for scaling.
+        std_train = training_data_t.std(axis=0)
+        mean_train = training_data_t.mean(axis=0)
+        # Use scikit's preprocessing module to scale training data.
+        training_data_sc = preprocessing.scale(training_data_t)
+        # Scale test data based on mean and stdev of training data
+        test_data_sc = test_data_t.copy()
+        for i in range(len(test_data_sc)):
+            test_data_sc[i] = (test_data_sc[i] - mean_train) / std_train
+        # Perform classification.
+        clf = svm.SVC(kernel="linear")
+        clf.fit(training_data_sc, training_classes)
+        prediction = clf.predict(test_data_sc)
+        accuracies.append(accuracy_score(test_classes, prediction))
+        # print accuracy of the training of the selected features
+    return accuracies
 
 
-def svm(trainingFeatureData, testFeatureData, trainingClassType, testClassType):
-  #get the mean and std from the training data
-  stdDevTraining = trainingFeatureData.std(axis=0)
-  meanTraining = trainingFeatureData.mean(axis=0)
-  #use Scikit's preprocessing module to scale training data
-  trainingFeatureDataS = preprocessing.scale(trainingFeatureData)
-  #scale the test data using the mean and std from training
-  testFeatureDataS = np.array(testFeatureData, copy=True)
-  for i in range(len(testFeatureDataS)):
-    testFeatureDataS[i] = (testFeatureDataS[i] - meanTraining)/stdDevTraining
-    
-    
-  X = trainingFeatureDataS
-  y = trainingClassType
-  #use a linear classifer
-  clf = svm.SVC(kernel='linear')
-  clf.fit(X, y)  
-  #get the accuracy, precision, and recall
-  prediction = clf.predict(testFeatureDataS)
-  accuracy = accuracy_score(testClassType, prediction)
-  precision = precision_score(testClassType, prediction)
-  recall = recall_score(testClassType, prediction)
-  y_score = clf.decision_function(testFeatureDataS)
-  
-  #print ROC curve
-  fpr, tpr, thresholds = roc_curve(testClassType, y_score)
-  roc_auc = auc(fpr, tpr)
-  fig, ax = plt.subplots()
-  plt.plot(fpr, tpr, color='red', label='ROC curve (area = %0.2f)' % roc_auc)
-  plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
-  plt.xlim([-0.01, 1.05])
-  plt.ylim([-0.01, 1.05])
-  plt.xlabel('False Positive Rate')
-  plt.ylabel('True Positive Rate')
-  plt.title('ROC Curve Experiment 1')
-  plt.legend(loc="lower right")
-  
-  textstr = '\n'.join((
-    r'$Accuracy=%.2f$' % (accuracy, ),
-    r'$Precision=%.2f$' % (precision, ),
-    r'$Recall=%.2f$' % (recall, )))
-  props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-  
-  # place a text box with accuracy, precision, and recall
-  ax.text(0.8, 0.3, textstr, fontsize=14, verticalalignment='top', bbox=props)
-  plt.draw()
-  return clf.coef_ #return weight vector
+def random_feature_selection(
+    training_features, test_features, training_classes, test_classes,
+):
+    """Performs SVM classifaction using random features."""
+    random_features = np.arange(57)
+    # Randomize indices to access when training.
+    np.random.shuffle(random_features)
+    # Perform features training based on these random features.
+    accuracies = features_training(
+        training_features,
+        test_features,
+        training_classes,
+        test_classes,
+        random_features,
+    )
+    features = 2
+    for acc in accuracies:
+        print(
+            "Using ",
+            features,
+            "random features, SVM achieves an accuracy " "of",
+            acc,
+        )
+        features += 1
+
+
+def weight_feature_selection(
+    training_features, test_features, training_classes, test_classes, weights,
+):
+    """Performs SVM classifaction using best features."""
+    sorted_features = np.argsort(weights)[::-1]
+    # Perform features training based on these sorted indices
+    accuracies = features_training(
+        training_features,
+        test_features,
+        training_classes,
+        test_classes,
+        sorted_features,
+    )
+    features = 2
+    for acc in accuracies:
+        print(
+            "Using top",
+            features,
+            "features, SVM achieves an accuracy " "of",
+            acc,
+        )
+        features += 1
+
+
+def svm_train(
+    training_features, test_features, training_classes, test_classes,
+):
+    """Gets weights from SVM training."""
+    # Get the mean and std from the training data.
+    std_train = training_features.std(axis=0)
+    mean_train = training_features.mean(axis=0)
+    # Scale the test data using the mean and std from training data.
+    test_features_sc = test_features.copy()
+    for i in range(len(test_features_sc)):
+        test_features_sc[i] = (test_features_sc[i] - mean_train) / std_train
+    # Use Scikit's preprocessing module to scale training data.
+    X = preprocessing.scale(training_features)
+    y = training_classes
+    # Use a linear classifier.
+    clf = svm.SVC(kernel="linear")
+    clf.fit(X, y)
+    # Get the accuracy, precision, and recall.
+    prediction = clf.predict(test_features_sc)
+    accuracy = accuracy_score(test_classes, prediction)
+    precision = precision_score(test_classes, prediction)
+    recall = recall_score(test_classes, prediction)
+    y_score = clf.decision_function(test_features_sc)
+
+    # Plot ROC curve.
+    fpr, tpr, thresholds = roc_curve(test_classes, y_score)
+    roc_auc = auc(fpr, tpr)
+    fig, ax = plt.subplots()
+    plt.plot(fpr, tpr, color="red", label="ROC curve (area = %0.2f)" % roc_auc)
+    plt.plot([0, 1], [0, 1], color="navy", linestyle="--")
+    plt.xlim([-0.01, 1.05])
+    plt.ylim([-0.01, 1.05])
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC Curve")
+    plt.legend(loc="lower right")
+
+    textstr = "\n".join(
+        (
+            r"$Accuracy=%.2f$" % (accuracy,),
+            r"$Precision=%.2f$" % (precision,),
+            r"$Recall=%.2f$" % (recall,),
+        )
+    )
+    props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
+
+    # Place a text box with accuracy, precision, and recall.
+    ax.text(
+        0.8, 0.3, textstr, fontsize=14, verticalalignment="top", bbox=props
+    )
+    plt.draw()
+    # return weight vector
+    return clf.coef_
 
 
 def main():
-  features = []
-  classType = []
-  dataFile = open(sys.argv[1])
-  dataReader = csv.reader(dataFile, delimiter=',')
-  #append all features and classes from csv file
-  for row in dataReader:
-    features.append([float(i) for i in row[0:57]])
-    classType.append(int(row[-1]))
-  
-  features = np.asarray(features)
-  classType = np.asarray(classType)
-  
-  #use Scikit's built in data splitter 
-  trainingFeatureData, testFeatureData, trainingClassType, testClassType=train_test_split(features, classType, test_size=.5, random_state=None) 
-  
-  #get the weight vector using experiment's one training
-  expOneWeightVector = svm(trainingFeatureData, testFeatureData, trainingClassType, testClassType)[0]
-  print("Weight based Feature selection")
-  #use this weight vector to perform feature selection based on weight
-  weightFeatureSelection(trainingFeatureData, testFeatureData, trainingClassType, testClassType, expOneWeightVector)
-  print("Random Feature selection")
-  #perform random feature selection
-  randomFeatureSelection(trainingFeatureData, testFeatureData, trainingClassType, testClassType)
-  #plot ROC curve from experiment one
-  plt.show()
-  
+    features = []
+    class_type = []
+    data_reader = csv.reader(open("spambase.csv"), delimiter=",")
+    # Get all features and classes from csv file.
+    for row in data_reader:
+        features.append([float(i) for i in row[0:57]])
+        class_type.append(int(row[-1]))
 
-if __name__ == '__main__':
-  if len(sys.argv) > 1:
+    features = np.asarray(features)
+    class_type = np.asarray(class_type)
+
+    # Use Scikit's built in data splitter.
+    (
+        training_features,
+        test_features,
+        training_classes,
+        test_classes,
+    ) = train_test_split(
+        features, class_type, test_size=0.5, random_state=None
+    )
+
+    # Get weights from SVM training.
+    weights = svm_train(
+        training_features, test_features, training_classes, test_classes,
+    )[0]
+    print("Weight based Feature selection")
+    # use this weight vector to perform feature selection based on weight
+    weight_feature_selection(
+        training_features,
+        test_features,
+        training_classes,
+        test_classes,
+        weights,
+    )
+    print("Random Feature selection")
+    # perform random feature selection
+    random_feature_selection(
+        training_features, test_features, training_classes, test_classes,
+    )
+    # plot ROC curve from experiment one
+    plt.show()
+
+
+if __name__ == "__main__":
     main()
-  else:
-    print("Needs to provide spambase.csv as argument")
